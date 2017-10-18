8d4b6c7b4b24aa62317bf3412b2a216d8819b44b
Svetlozar Georgiev <sfe7lio@gmail.com>
2017-10-18 20:38:47 +0100

    kings can now jump in any direction and move correctly

--- src/Controller/MoveController.java
+++ src/Controller/MoveController.java
@@ -15,89 +15,48 @@ public class MoveController {
         board = b;
     }
 
-    //returns true if a regular move (move diagonally one tile) is legal
-      /*public boolean isMoveLegal(int gridX, int gridY, int destX, int destY) {
-        if(board.isTileOccupied(gridX, gridY) && !board.isTileOccupied(destX, destY)) { //if source has a piece and destination is an empty tile
-            Piece piece = board.getPiece(gridX, gridY);
-            if(piece.getType() == Type.WHITE) {
-                //legal moves for white pieces:
-                //move diagonally to the left
-                if(destX - gridX == 1 && destY - gridY == -1) {
-                    return true;
-                } else if (destX - gridX == 1 && destY - gridY == 1) { //move diagonally to the right
-                    return true;
-                }
-            } else if (piece.getType() == Type.BLACK) {
-                //legal moves for black, in the opposite direction:
-                //move diagonally to the left
-                if(destX - gridX == -1 && destY - gridY == -1) {
-                    return true;
-                } else if (destX - gridX == -1 && destY - gridY == 1) { //move diagonally to the right
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    //TODO improve this
-    public boolean isMoveJump(int row, int col, int destRow, int destCol) {
-        try {
-            if( (board.isTileOccupied(row, col) && board.getPiece(row, col).getType() == Type.WHITE) && (board.isTileOccupied(row+1, col-1)
-                    && board.getPiece(row+1, col-1).getType() == Type.BLACK) && !board.isTileOccupied(row+2, col-2) ) {
-                board.movePiece(row, col, row+2, col-2);
-                board.removePiece(row+1, col-1);
-                return true;
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Out of bounds exception.");
-        }
-
-        return false;
-    } */
-
     //this should be a separate method because we don't want to have one huge method that does everything
     //TODO can this be done in a different way without so many if...else statements?
     public ArrayList<GridPosition> getPossibleJumps(int row, int col) {
         ArrayList<GridPosition> jumps = new ArrayList<>();
         Piece piece = board.getPiece(row, col);
         try {
-            if (piece.getType() == Type.WHITE || piece.getType() == Type.WHITE_KING || piece.getType() == Type.BLACK_KING) {
+            if (piece.getType() == Type.WHITE || piece.getType() == Type.WHITE_KING || piece.getType() == Type.BLACK_KING ) {
                 //if(board.isLegalPos(row + 1, col - 1))
                     if (board.isTileOccupied(row, col) && ( board.isTileOccupied(row + 1, col - 1)
-                            && board.getPiece(row + 1, col - 1).getType() == Type.BLACK) && !board.isTileOccupied(row + 2, col - 2)) {
+                            && board.getPiece(row + 1, col - 1).getType() == getOppositeType(piece.getType())) && !board.isTileOccupied(row + 2, col - 2)) {
                         jumps.add(new GridPosition(row + 2, col - 2));
                     }
                 //if(board.isLegalPos(row + 1, col + 1))
                     if (board.isTileOccupied(row, col) && (board.isTileOccupied(row + 1, col + 1)
-                            && board.getPiece(row + 1, col + 1).getType() == Type.BLACK) && !board.isTileOccupied(row + 2, col + 2)) {
+                            && board.getPiece(row + 1, col + 1).getType() == getOppositeType(piece.getType())) && !board.isTileOccupied(row + 2, col + 2)) {
                         jumps.add(new GridPosition(row + 2, col + 2));
                     }
             }
-            if(piece.getType() == Type.BLACK || piece.getType() == Type.WHITE_KING || piece.getType() == Type.BLACK_KING){
+
+            if(piece.getType() == Type.BLACK || piece.getType() == Type.BLACK_KING || piece.getType() == Type.WHITE_KING) {
                 //if(board.isLegalPos(row - 1, col - 1))
-                    if (board.isTileOccupied(row, col) && (board.isTileOccupied(row - 1, col - 1) && board.getPiece(row - 1, col - 1).getType() == Type.WHITE)
+                    if (board.isTileOccupied(row, col) && (board.isTileOccupied(row - 1, col - 1) && board.getPiece(row - 1, col - 1).getType() == getOppositeType(piece.getType()))
                             && !board.isTileOccupied(row - 2, col - 2)) {
                         jumps.add(new GridPosition(row - 2, col - 2));
                     }
                 //if(board.isLegalPos(row - 1, col + 1))
-                    if(board.isTileOccupied(row, col) && (board.isTileOccupied(row - 1, col + 1) && board.getPiece(row - 1, col + 1).getType() == Type.WHITE)
+                    if(board.isTileOccupied(row, col) && (board.isTileOccupied(row - 1, col + 1) && board.getPiece(row - 1, col + 1).getType() == getOppositeType(piece.getType()))
                             && !board.isTileOccupied(row-2, col+2)) {
                         jumps.add(new GridPosition(row-2, col+2));
                     }
             }
 
         } catch(ArrayIndexOutOfBoundsException e) {
-            //System.out.println("getPossibleJumps array out of bounds!");
+            System.out.println("getPossibleJumps array out of bounds!");
         } catch(NullPointerException e) {
-            //System.out.println("getPossibleJumps null pointer!");
+            System.out.println("getPossibleJumps null pointer!");
         }
 
         return jumps;
     }
 
-    //
+    //return an ArrayList with all the possible regular moves for a piece
     public ArrayList<GridPosition> getPossibleMoves(int row, int col) {
         ArrayList<GridPosition> moves = new ArrayList<>();
         try {
@@ -155,6 +114,7 @@ public class MoveController {
         return canJump;
     }
 
+    //undoes the last play
     public static void undoLastMove() {
         GridPosition[] gps;
         if(!GameHistory.getMoves().isEmpty()) {
@@ -174,5 +134,13 @@ public class MoveController {
         }
     }
 
+    //returns the opposite type
+    private Type getOppositeType(Type t) {
+        if(t == Type.WHITE || t == Type.WHITE_KING) {
+            return Type.BLACK;
+        }
+
+        return Type.WHITE;
+    }
 
 }
